9cd5f7b276e0124f0da326b07d25bfbd820e1b89
Marcelo-vta <mclonso123@outlook.com>
2023-10-24 16:15:11 -0300

    tela inicial apagada

--- classes_telas.py
+++ classes_telas.py
@@ -2,64 +2,6 @@ import pygame
 from classes_personagens import Jogador, Boss1, Boss2, Boss3
 from classes_objetos import Lava, Parede, Veneno, MateriaEscura
 
-class TelaInicial:
-    def __init__(self, state, assets):
-        self.largura_tela = state['tela_dimen'][0]
-        self.altura_tela = state['tela_dimen'][1]
-        self.fonte_padrao = pygame.font.get_default_font() # Carrega a fonte padrão
-        self.font_texto = pygame.font.Font(self.fonte_padrao, 16)
-        self.font_nome_jogo = pygame.font.Font(self.fonte_padrao, 20)
-        self.ret_largura = 100
-        self.ret_altura = 35
-        self.ret_x = self.largura_tela // 2 - self.ret_largura // 2
-        self.ret_y = self.altura_tela // 2 + self.ret_altura // 2
-        self.ret_cor = (255, 0, 0)
-        self.botòes = []
-
-        for i in range(2):
-            self.botòes.append([self.ret_x, self.ret_y])
-            self.ret_y += 50
-
-
-    def atualiza_estado(self):
-        for event in pygame.event.get(): # Retorna uma lista com todos os eventos que ocorreram desde a última vez que essa função foi chamada
-            if event.type == pygame.QUIT: 
-                return -1, False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    pass
-                pos_mouse = pygame.mouse.get_pos()
-                
-                for indice, botão in enumerate(self.botòes):
-                    rect = pygame.Rect(botão[0], botão[1], self.ret_largura, self.ret_altura)
-
-                    if rect.collidepoint(pos_mouse) and indice == 0:
-                        return 1, False
-                    elif rect.collidepoint(pos_mouse) and indice == 1:
-                        return 1, True
-            
-        return 0, False
-
-    
-    def desenha(self, window):
-        window.fill((0, 0, 0)) # Prrenche a janela do jogo com a cor preta
-
-        nome_jogo = self.font_nome_jogo.render('NOME DO JOGO', True, (0, 0, 255)) # Cria uma imagem do texto
-        window.blit(nome_jogo, ( self.largura_tela // 2 - 100, self.altura_tela // 2 + 55)) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
-
-        vertice_ret_1 = ((self.botòes[0][0], self.botòes[0][1]), (self.botòes[0][0] + self.ret_largura, self.botòes[0][1]), (self.botòes[0][0] + self.ret_largura, self.botòes[0][1] + self.ret_altura), (self.botòes[0][0], self.botòes[0][1] + self.ret_altura))
-        pygame.draw.polygon(window, self.ret_cor, vertice_ret_1)
-        texto_play1 = self.font_texto.render('JOGAR MODO NORMAL', True, (0, 0, 0)) # Cria uma imagem do texto
-        window.blit(texto_play1, (self.botòes[0][0] + 5, self.botòes[0][1] + 5)) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
-
-        vertice_ret_1 = ((self.botòes[1][0], self.botòes[1][1]), (self.botòes[1][0] + self.ret_largura, self.botòes[1][1]), (self.botòes[1][0] + self.ret_largura, self.botòes[1][1] + self.ret_altura), (self.botòes[1][0], self.botòes[1][1] + self.ret_altura))
-        pygame.draw.polygon(window, self.ret_cor, vertice_ret_1)
-        texto_play1 = self.font_texto.render('JOGAR MODO RITIMADO', True, (0, 0, 0)) # Cria uma imagem do texto
-        window.blit(texto_play1, (self.botòes[1][0] + 5, self.botòes[1][1] + 5)) # Desenha a imagem já carregada por pygame.image.load em window na posição (x, y).
-
-        pygame.display.update() # Atualiza a janela do jogo
-
-
 class RoomBegin:
     def __init__(self, state, assets):
         self.largura_tela = state['tela_dimen'][0]
@@ -71,7 +13,7 @@ class RoomBegin:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 1
+        return 0
 
     
     def desenha(self, window):
@@ -91,7 +33,7 @@ class RoomBoss1:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 2
+        return 1
 
     
     def desenha(self, window):
@@ -111,7 +53,7 @@ class RoomBoss2:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 3
+        return 2
 
     
     def desenha(self, window):
@@ -130,7 +72,7 @@ class RoomBoss3:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 4
+        return 3
 
     
     def desenha(self, window):
@@ -150,7 +92,7 @@ class RoomFinal:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 5
+        return 4
 
     
     def desenha(self, window):
@@ -172,7 +114,7 @@ class TelaFinal:
             if event.type == pygame.QUIT: 
                 return -1
         
-        return 6
+        return 5
             
     
     def desenha(self, window):
@@ -197,7 +139,7 @@ class TelaGameOver:
             if event.type == pygame.QUIT: 
                 return -1
             
-        return 7
+        return 6
         
     
     def desenha(self, window):
