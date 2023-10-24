52f82c1ef69b57036db92196a3acf931b1952107
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-10-24 11:01:10 -0300

    Criando todas as classes

---
+++ classes_telas.py
@@ -0,0 +1,48 @@
+class RoomBegin:
+    def __init__(self):
+        
+
+    def atualiza_estado(self):
+
+    
+    def desenha(self, window):
+
+
+class RoomBoss1:
+    def __init__(self):
+        
+
+    def atualiza_estado(self):
+
+    
+    def desenha(self, window):
+
+
+class RoomBoss2:
+    def __init__(self):
+        
+
+    def atualiza_estado(self):
+
+    
+    def desenha(self, window):
+
+
+class RoomBoss3:
+    def __init__(self):
+        
+
+    def atualiza_estado(self):
+
+    
+    def desenha(self, window):
+
+
+        class RoomFinal:
+    def __init__(self):
+        
+
+    def atualiza_estado(self):
+
+    
+    def desenha(self, window):
