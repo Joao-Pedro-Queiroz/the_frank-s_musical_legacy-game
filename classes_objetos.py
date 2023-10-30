4f3cf53943028aa2e63d937f73c08a2760095ce6
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-10-30 14:20:11 -0300

    alterando a init da classe paredes

--- classes_objetos.py
+++ classes_objetos.py
@@ -22,14 +22,21 @@ class Tiro(pygame.sprite.Sprite):
         pass
 
 class Parede(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos_x, pos_y, largura, altura):
         '''
         Função que define a classe Parede
 
         parâmetro self: representa a própria classe
+        parâmetro pos_x: representa a posição x do nato superior esquerdo da parede
+        parâmetro pos_y: representa a posição y do nato superior esquerdo da parede
+        parâmetro largura: representa a largura da parede
+        parâmetro altura: representa a altura da parede
         ''' 
-
+    
         pygame.sprite.Sprite.__init__(self)
+
+        self.rect = pygame.Rect(pos_x, pos_y, largura, altura)
+        
 
     def update(self):
         pass
